d050a0e94382530549a40e71a7ee2d988ae71552
anshuln <alexander.mathiasen@gmail.com>
2019-06-11 16:52:25 +0200

    Adds coupling layers and inverse computation

--- baseline.py
+++ baseline.py
@@ -23,8 +23,13 @@ shape = X[0].shape
 
 
 # [ - 0.5 , 0.5  ]
-X = X / 255 - 0.5 
+X = X / 1 - 127.5 
+std_dev = 2**8
 
+def prettify(x): #Scales images between 0,1
+	x = x - x.min()
+	x = x / x.max()
+	return x
 
 class Sequential(tf.keras.Sequential): 
 
@@ -54,11 +59,17 @@ class Conv(tf.keras.layers.Layer):
 		X = tf.math.real(X)
 		return X
 
+
 	def call_inv(self, X): 
 		# return X
 		X = tf.cast(X, dtype=tf.complex64)
 		X = tf.signal.fft3d(X * self.scale ) # self.scale correctly 
+		#The next 2 lines are a redundant computation necessary because w needs to be an EagerTensor for the output to be eagerly executed, and that was not the case earlier
+		self.w 	= tf.cast(self.w_real, dtype=tf.complex64)
+		self.w 	= tf.signal.fft3d(self.w / self.scale)
+
 		X = X / self.w
+
 		X = tf.signal.ifft3d(X / self.scale)   
 		X = tf.math.real(X)
 		return X
@@ -78,62 +89,119 @@ class Conv(tf.keras.layers.Layer):
 		#	zeros[0,0,0] = 1 
 		#	return zeros
 
-		self.w_real 	= self.add_variable(name="w",shape=input_shape[1:], initializer=identitiy_initializer_real, trainable=True)
+		self.w_real 	= self.add_variable(name="w_real",shape=input_shape[1:], initializer=identitiy_initializer_real, trainable=True)
 		self.w 	= tf.cast(self.w_real, dtype=tf.complex64)	#hacky way to initialize real w and actual w, since tf does weird stuff if 'variable' is modified
 		self.w 	= tf.signal.fft3d(self.w / self.scale)
-		# self.num_outputs = input_shape
-		# print("building",self.w)
 		
 
 	def compute_output_shape(self, input_shape): 
 		return tf.TensorShape(input_shape[1:])
 
+def ReLU(x): return tf.math.maximum( x, 0 )
 
-def nll(y_pred, y_true): 
+
+class UpperCoupledReLU(tf.keras.layers.Layer): 
+
+
+	def call(self, inputs): 		
+		_, h, w, c = inputs.shape
+
+		# assumes c is even
+		assert c % 2 == 0, "The non-linearity assumes that c is even, it is not: c=%i"%c
+
+		x1 = inputs[:, :, :, :c//2]
+		x2 = inputs[:, :, :, c//2:]
+
+		x2 = x2 + ReLU(x1)
+		
+		return tf.concat((x1, x2), axis=-1)
+
+
+	def call_inv(self, outputs): 	
+		_, h, w, c = outputs.shape
+
+		# assumes c is even
+		assert c % 2 == 0, "The non-linearity assumes that c is even, it is not: c=%i"%c
+
+		x1 = outputs[:, :, :, :c//2]
+		x2 = outputs[:, :, :, c//2:]
+
+		x2 = x2 - ReLU(x1)
+		
+		return tf.concat((x1, x2), axis=-1)
+
+	def log_det(self): 				return 0. 
+
+
+class LowerCoupledReLU(UpperCoupledReLU): 
+
+	def call(self, inputs): 	return super(LowerCoupledReLU, self).call_inv(inputs)
+	def call_inv(self, outputs): return super(LowerCoupledReLU, self).call(outputs)			
+
+
+class Squeeze(tf.keras.layers.Layer): 
+	def build(self, input_shape): 
+		_, self.h, self.w, self.c = input_shape
+
+	def call(self, inputs):
+		h, w, c = self.h, self.w, self.c
+		out = tf.reshape(  inputs,     (-1, h//2, 2, w//2, 2, c))
+		out = tf.transpose(out,        (0, 1, 3, 2, 4, 5))
+		return tf.reshape(out,      (-1, h//2, w//2, c * 2 * 2))
+
+	def call_inv(self, inputs):
+		h, w, c = self.h, self.w, self.c
+		out = tf.reshape(    inputs, (-1, h//2, w//2, 2, 2, c))
+		out = tf.transpose(  out,    (0, 1, 3, 2, 4, 5))
+		out = tf.reshape(    out,    (-1, h, w, c))
+		return out 
+
+	def compute_output_shape(self, input_shape): 
+		n, h, w, c = input_shape
+		return (n, h//2, w//2, c*4)
+
+	def log_det(self): return 0.
+
+
+def log_normal_density(x): return tf.math.reduce_sum( -1/2 * (x**2/std_dev**2 + tf.math.log(2*np.pi*std_dev**2)) )
+
+def nll(y_true,y_pred): 	#TODO add scaling penalties?
 	logdet = model.log_det()
 	print(y_pred, y_true, logdet)
 
-	normal = 0#normal_distributon(y_pred) 
+	normal = log_normal_density(y_pred) 
 
-	return logdet + normal
+	return -(logdet + normal)
 	
 
-#input = tf.keras.Input(shape=shape)
-#flow = Conv()(input)
-
-#model = Model(inputs=input, outputs=input)
 
 model = Sequential()
 
+model.add(Squeeze())
 model.add(Conv())
-model.add(Conv())
+model.add(UpperCoupledReLU())
 model.compile(optimizer=tf.optimizers.Adam(0.001), loss=nll)
 
 pred 	= model.predict(X[:2])
-# print(model.layers[0].trainable_variables)
-rec = model.predict_inv(pred)
-# tensor = tf.ones([3,3])
-
-# print("-"*100)
-# print(tensor)
-
-
-# assert False
-
-print(rec)
 
 model.summary()
+model.fit(X[:20000],X[:20000],epochs=5)
+pred1 	= model.predict(X[:2])
+rec = model.predict_inv(pred1)
+
+
+
 fig, ax = plt.subplots(1, 3)
 
-ax[0].imshow(X[0].reshape(32,32,3))
-ax[1].imshow(pred[0].reshape(32,32,3))
-# ax[2].imshow(rec[0].reshape(32,32,3))
+ax[0].imshow(prettify(X[1].reshape(32,32,3)))
+ax[1].imshow(prettify(pred1[1].reshape(32,32,3)))
+ax[2].imshow(prettify(rec.numpy()[1].reshape(32,32,3)))
 
 
 
 
 
-plt.pause(10**6)
+plt.show()
 		
 
 
