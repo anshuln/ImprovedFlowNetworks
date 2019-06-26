af467f61d513cc47812179e95bed4bdb5b7a5633
anshuln <anshulnasery@gmail.com>
2019-06-26 14:50:17 +0200

    Merge commit

---
+++ gradient_checkpointing.py
@@ -0,0 +1,359 @@
+import os
+
+import tensorflow as tf 
+import numpy as np 
+
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_VLOG_LEVEL']='3'
+'''
+TODO - Make a function to compute gradients effficiently in model class  -- DONE
+	   This should call comput grads in all layers, make fucntions accordingly -- DONE
+	   Cleaning	-- In Progress
+	   intermediate layer grad computation -- DONE
+	   iteratively update weights --DONE
+Adapted version of this code - https://github.com/tensorflow/tensorflow/tree/master/tensorflow/contrib/eager/python/examples/revnet
+'''
+
+std_dev = 2**8
+batch_size = 32
+def prettify(x): #Scales images between 0,1
+	x = x - x.min()
+	x = x / x.max()
+	return x
+
+class FlowSequential(tf.keras.Sequential):
+	
+	def __init__(self):
+		super(FlowSequential,self).__init__()
+	# 	self.saved_hidden = []   #Will store the input and activations of the last layer similar to gradient checkpointing 
+	# 							 #TODO convert this to a tensor instead of list for GPU storage
+
+	def loss_function(self,X):	#nll loss
+		def log_normal_density(x): return tf.math.reduce_sum( -1/2 * (x**2/std_dev**2 + tf.math.log(2*np.pi*std_dev**2)) )
+
+		log_det = self.log_det()	#This can be thought of as a regularizer...
+		normal = log_normal_density(X)
+		return -(log_det+normal) 
+
+	def predict_inv(self,X):
+		for layer in self.layers[::-1]:
+			X = layer.call_inv(X)
+		return X
+
+	def get_last_inp(self,X,training=True):		#TODO - cleanup this function
+		y1 = X
+		for layer in self.layers[:-1]:
+			# print('forward',y1[0])
+			y1 = layer.call(y1)
+
+		return y1
+
+	def log_det(self): 
+		det = 0.
+		for layer in self.layers: 
+			if isinstance(layer, tf.keras.layers.InputLayer): continue 
+			det = tf.add(det,layer.log_det())
+		return det
+
+	def compute_and_apply_gradients(self,X,optimizer=None):
+		'''
+		Computes gradients efficiently and updates weights
+		Returns - Loss on the batch
+		'''
+		x = self.call(X)		#I think putting this in context records all operations onto the tape, thereby destroying purpose of checkpointing...
+		last_layer = self.layers[-1]
+		#Computing gradients of loss function wrt the last acticvation
+		with tf.GradientTape() as tape:
+			tape.watch(x)
+			loss = self.loss_function(x)	#May have to change
+		grads_combined = tape.gradient(loss,[x])
+		dy = grads_combined[0]
+		y = x
+		#Computing gradients for each layer
+		for layer in self.layers[::-1]:		
+			x = layer.call_inv(y)
+			dy,grads = layer.compute_gradients(x,dy,layer.log_det)
+			optimizer.apply_gradients(zip(grads,layer.trainable_variables))
+			y = x 
+		return loss
+
+	def other_grads(self,X):
+		'''
+		Helper function to get 'True Gradients' of the network
+		'''
+		with tf.GradientTape() as tape:
+			loss = self.loss_function(self.call(X))
+		grads = tape.gradient(loss,self.trainable_variables)
+		return grads,loss
+
+class LayerWithGrads(tf.keras.layers.Layer):	#Virtual Class
+	def __init__(self):
+		super(LayerWithGrads,self).__init__()
+
+	def call(self,X):
+		raise NotImplementedError
+
+	def call_inv(self,X):
+		raise NotImplementedError
+
+	def compute_gradients(self,x,dy,regularizer=None):	
+		'''
+		Computes gradients for backward pass
+		Args:
+			x - tensor compatible with forward pass, input to the layer
+			dy - incoming gradient from backprop
+			regularizer - function, indicates dependence of loss on weights of layer
+		Returns
+			dy - gradients wrt input, to be backpropagated
+			grads - gradients wrt weights
+		'''
+		with tf.GradientTape() as tape:
+			tape.watch(x)
+			y_ = self.call(x)	#Required to register the operation onto the gradient tape
+		grads_combined = tape.gradient(y_,[x]+self.trainable_variables,output_gradients=dy)
+		dy,grads = grads_combined[0],grads_combined[1:]
+
+		if regularizer is not None:
+			with tf.GradientTape() as tape:
+				reg = -regularizer()
+			grads_wrt_reg = tape.gradient(reg, self.trainable_variables)
+			grads = [a[0]+a[1] for a in zip(grads,grads_wrt_reg)]
+		return dy,grads
+
+class Conv(LayerWithGrads): 
+
+	def __init__(self,trainable=True): 
+		self.built = False
+		super(Conv, self).__init__()
+	
+
+	def call(self, X): 
+		if self.built == False:    #For some reason the layer is not being built without this line
+			self.build(X.get_shape().as_list())
+
+		#The next 2 lines are a redundant computation necessary because w needs to be an EagerTensor for the output to be eagerly executed, and that was not the case earlier
+		#EagerTensor is required for backprop to work...
+		#Further, updating w_real will automatically trigger an update on self.w, so it is better to not store w at all
+		#TODO - figure out a way to avoid, or open an issue with tf...
+		self.w 	= tf.cast(self.w_real, dtype=tf.complex64)
+		self.w 	= tf.signal.fft3d(self.w / self.scale)
+
+		X = tf.cast(X, dtype=tf.complex64)
+		X = tf.signal.fft3d(X / self.scale) 
+		X = X * self.w
+		X = tf.signal.ifft3d(X * self.scale ) 
+		X = tf.math.real(X)
+		return X
+
+
+	def call_inv(self, X): 
+		X = tf.cast(X, dtype=tf.complex64)
+		X = tf.signal.fft3d(X * self.scale ) # self.scale correctly 
+		#The next 2 lines are a redundant computation necessary because w needs to be an EagerTensor for the output to be eagerly executed, and that was not the case earlier
+		self.w 	= tf.cast(self.w_real, dtype=tf.complex64)
+		self.w 	= tf.signal.fft3d(self.w / self.scale)
+
+		X = X / self.w
+
+		X = tf.signal.ifft3d(X / self.scale)   
+		X = tf.math.real(X)
+		return X
+
+	def log_det(self): 	return tf.math.reduce_sum(tf.math.log(tf.math.abs(tf.signal.fft3d(tf.cast(self.w_real/self.scale,dtype=tf.complex64)))))	#Need to return EagerTensor
+
+
+	def build(self, input_shape): 
+		self.scale = np.sqrt(np.prod(input_shape[1:])) # np.sqrt(np.prod([a.value for a in input_shape[1:]]))
+
+		# todo; change to [[[1, 0000],[0000], [000]] 
+
+		def identitiy_initializer_real(shape, dtype=None):
+			return (tf.math.real(tf.signal.ifft3d(tf.ones(shape, dtype=tf.complex64)*self.scale))) 
+
+		self.w_real 	= self.add_variable(name="w_real",shape=input_shape[1:], initializer=identitiy_initializer_real, trainable=True)
+		# self.w 	= tf.cast(self.w_real, dtype=tf.complex64)	#hacky way to initialize real w and actual w, since tf does weird stuff if 'variable' is modified
+		# self.w 	= tf.signal.fft3d(self.w / self.scale)
+		self.built = True
+
+		
+
+	def compute_output_shape(self, input_shape): 
+		return tf.TensorShape(input_shape[1:])
+
+def ReLU(x): return tf.math.maximum( x, 0 )
+
+
+class UpperCoupledReLU(LayerWithGrads): 		
+	def __init__(self,trainable=True): 
+		self.built = False
+		super(UpperCoupledReLU, self).__init__()
+
+	def call(self, inputs): 		
+		_, h, w, c = inputs.shape
+
+		# assumes c is even
+		assert c % 2 == 0, "The non-linearity assumes that c is even, it is not: c=%i"%c
+
+		x1 = inputs[:, :, :, :c//2]
+		x2 = inputs[:, :, :, c//2:]
+
+		x2 = x2 + ReLU(x1)
+		
+		return tf.concat((x1, x2), axis=-1)
+
+
+	def call_inv(self, outputs): 	
+		_, h, w, c = outputs.shape
+
+		# assumes c is even
+		assert c % 2 == 0, "The non-linearity assumes that c is even, it is not: c=%i"%c
+
+		x1 = outputs[:, :, :, :c//2]
+		x2 = outputs[:, :, :, c//2:]
+
+		x2 = x2 - ReLU(x1)
+		
+		return tf.concat((x1, x2), axis=-1)
+
+	def log_det(self): 				return tf.zeros((1,)) 
+
+
+class LowerCoupledReLU(UpperCoupledReLU): 
+	def __init__(self,trainable=True): 
+		self.built = False
+		super(LowerCoupledReLU, self).__init__()
+
+	def call(self, inputs): 	return super(LowerCoupledReLU, self).call_inv(inputs)
+	def call_inv(self, outputs): return super(LowerCoupledReLU, self).call(outputs)			
+
+
+
+class Squeeze(LayerWithGrads): 
+	def __init__(self,trainable=True): 
+		self.built = False
+		super(Squeeze, self).__init__()
+	
+
+	def build(self, input_shape): 
+		_, self.h, self.w, self.c = input_shape
+		self.built = True
+
+	def call(self, inputs):
+		if self.built == False:    #For some reason the layer is not being built without this line
+			self.build(inputs.get_shape().as_list())
+		h, w, c = self.h, self.w, self.c
+		out = tf.reshape(  inputs,     (-1, h//2, 2, w//2, 2, c))
+		out = tf.transpose(out,        (0, 1, 3, 2, 4, 5))
+		return tf.reshape(out,      (-1, h//2, w//2, c * 2 * 2))
+
+	def call_inv(self, inputs):
+		h, w, c = self.h, self.w, self.c
+		out = tf.reshape(    inputs, (-1, h//2, w//2, 2, 2, c))
+		out = tf.transpose(  out,    (0, 1, 3, 2, 4, 5))
+		out = tf.reshape(    out,    (-1, h, w, c))
+		return out 
+
+	def compute_output_shape(self, input_shape): 
+		n, h, w, c = input_shape
+		return (n, h//2, w//2, c*4)
+
+	def log_det(self): return tf.zeros((1,))
+
+
+def train_for_one_iter(model,X,optimizer,batch_size=batch_size):	#TODO implement mini-batch gradient descent here
+	'''
+	Trains the model on data for one iteration
+	Args:
+		model - model of class FlowSequential
+		X - Data of type np.ndarray, having (num_samples,(sample_size)) dims
+		optimizer - tf optimizer to use
+		batch_size - int
+	'''
+	num_batches = X.shape[0] // batch_size
+	X = np.random.permutation(X)
+	#Minibatch gradient descent
+	for i in tqdm(range(0,X.shape[0]-X.shape[0]%batch_size,batch_size)):	
+		# grads,loss = model.compute_gradients(X[i:(i+batch_size)])
+		losses = []
+		loss = model.compute_and_apply_gradients(X[i:(i+batch_size)],optimizer)
+		losses.append(loss.numpy())
+	loss = np.mean(losses)	
+
+	# loss = model.compute_and_apply_gradients(X,optimizer)
+	# loss = loss.numpy()
+	
+	return loss
+
+
+def main():
+	(X, _), (X_test, _) = tf.keras.datasets.cifar10.load_data() 	#This is actually numpy
+	print(X.dtype)
+	shape = X[0].shape
+
+
+	# [ - 0.5 , 0.5  ]
+	X = (X - 127.5)
+	std_dev = 2**8
+
+	model = FlowSequential()
+
+	model.add(Squeeze())
+	model.add(Conv())
+	model.add(UpperCoupledReLU())
+
+	# model.add(LowerCoupledReLU())
+
+	# model.add(Conv())
+	# model.add(UpperCoupledReLU())
+
+	model.add(Conv())
+	model.add(LowerCoupledReLU())
+
+	model.add(Conv())
+	model.add(UpperCoupledReLU())
+
+	model.add(Conv())
+	model.add(LowerCoupledReLU())
+
+	# model.add(Conv())
+	# model.add(UpperCoupledReLU())
+
+	# model.add(Conv())
+	# model.add(LowerCoupledReLU())
+
+	# model.add(Conv())
+	# model.add(UpperCoupledReLU())
+
+	# model.add(Conv())
+	# model.add(LowerCoupledReLU())
+
+	# print(model.layers)
+
+	epochs = 100
+	fast = 10000
+	optimizer = tf.optimizers.Adam(0.001)
+	pred1 	= model.predict(X[:1])	#Don't remove,essential to build the layers...
+	losses = []
+
+	for it in range(epochs):
+		loss = train_for_one_iter(model,X[:fast],optimizer)
+		print('Epoch : ',it,'Loss : ',loss)
+		losses+=([loss])
+	fixed_noise = tf.random.normal((1,16,16,12),0,std_dev)
+
+	pred1 	= model.call(X[:2])
+	rec1 = model.predict_inv(pred1)
+	rec = model.predict_inv(fixed_noise[:1])
+	plt.plot(np.arange(len(losses)),losses)
+	plt.show()
+	fig, ax = plt.subplots(1, 3)
+	ax[0].imshow(prettify(X[1].reshape(32,32,3)))
+	ax[1].imshow(prettify(rec1[1].numpy().reshape(32,32,3)))
+	ax[2].imshow(prettify(rec.numpy()[0].reshape(32,32,3)))
+	plt.show()
+
+if __name__ == '__main__':
+	main()
